ba77e090fc5e398a09450d11c2af296b44bf0de2
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2026-02-09 13:17:38 -0300

    fix: correct layout folder case for linux build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Layout from './components/Layout/Layout'
+import Layout from './components/layout/Layout'
 import Dashboard from './pages/Dashboard'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
